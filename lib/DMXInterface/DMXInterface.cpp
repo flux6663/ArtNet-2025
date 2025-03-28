c1de6bff3725fde1b67e29927f73f9ac32549049
MARTIN Mathis <contact@mathis-martin.fr>
2025-03-28 11:47:13 +0100

    rajout d'un flag lors d'un message mqtt

--- lib/DMXInterface/DMXInterface.cpp
+++ lib/DMXInterface/DMXInterface.cpp
@@ -5,6 +5,8 @@
 
 void initialiserDMX(uint8_t numeroPortDMX, uint8_t pinTranmissionDMX, uint8_t pinReceptionDMX, uint8_t pinRTS_DMX)
 {
+    dmx_set_pin(numeroPortDMX, pinTranmissionDMX, pinReceptionDMX, pinRTS_DMX);
+
     dmx_config_t config = DMX_CONFIG_DEFAULT;
 
     dmx_personality_t personalities[] = {
@@ -12,13 +14,11 @@ void initialiserDMX(uint8_t numeroPortDMX, uint8_t pinTranmissionDMX, uint8_t pi
 
     dmx_driver_install(numeroPortDMX, &config, personalities, PERSONALITER_DMX);
     dmx_driver_install(numeroPortDMX, &config, personalities, PERSONALITER_DMX);
-
-    dmx_set_pin(numeroPortDMX, pinTranmissionDMX, pinReceptionDMX, pinRTS_DMX);
 }
 
 void envoyerCanaux(const void *data)
 {
     dmx_write(PORT_DMX, data, DMX_PACKET_SIZE);
-    dmx_send_num(PORT_DMX, DMX_PACKET_SIZE);
     dmx_wait_sent(PORT_DMX, DMX_TIMEOUT_TICK);
+    dmx_send_num(PORT_DMX, DMX_PACKET_SIZE);
 }

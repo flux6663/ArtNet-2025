3843fe39a1832ab04b94a2ed8ced512ff4561d1e
MARTIN Mathis <contact@mathis-martin.fr>
2025-04-04 22:26:45 +0200

    création librairie configuration

---
+++ lib/configuration/configuration.cpp
@@ -0,0 +1,42 @@
+#include "configuration.h"
+#include <Arduino.h>
+
+AsyncWebServer server(80);
+Preferences preferences;
+
+void Configuration::setupServer() {
+
+  if(!SPIFFS.begin())
+  {
+    Serial.println("Erreur SPIFFS...");
+    return;
+  }
+
+  File root = SPIFFS.open("/");
+  File file = root.openNextFile();
+
+  while(file)
+  {
+    Serial.print("File: ");
+    Serial.println(file.name());
+    file.close();
+    file = root.openNextFile();
+  }
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+  {
+    request->send(SPIFFS, "/index.html", "text/html");
+  });
+
+  server.on("/w3.css", HTTP_GET, [](AsyncWebServerRequest *request)
+  {
+    request->send(SPIFFS, "/w3.css", "text/css");
+  });
+
+  server.on("/script.js", HTTP_GET, [](AsyncWebServerRequest *request)
+  {
+    request->send(SPIFFS, "/script.js", "text/javascript");
+  });
+
+  server.begin();     // Lance le serveur
+}
---
+++ lib/configuration/configuration.h
@@ -0,0 +1,19 @@
+#ifndef _CONFIGURATION_H__
+#define __CONFIGURATION_H__
+
+#include <Arduino.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <Preferences.h>
+#include <SPIFFS.h>
+
+class Configuration
+{
+    public:
+    void setupServer();
+
+    private:
+    
+};
+
+#endif
